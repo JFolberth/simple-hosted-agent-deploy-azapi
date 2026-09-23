9d472336d9de07e0d7cabdcb0695850bdc60578b
JFolberth <johnfolberth@gmail.com>
2026-09-23 16:58:45 +0000

    feat: Add CPU and memory variables to hosted agent configuration

--- simple_agent/azure/infra/hosted_agent/main.tf
+++ simple_agent/azure/infra/hosted_agent/main.tf
@@ -6,8 +6,10 @@ module "hosted_agent" {
   source = "../modules/hosted_agent"
 
   agent_name            = var.agent_name
+  cpu                   = var.cpu
   environment_variables = var.environment_variables
   image_uri             = local.image_uri
+  memory                = var.memory
   model_deployment_name = var.model_deployment_name
   project_endpoint      = var.project_endpoint
   rai_policy_id         = var.rai_policy_id
